fe747e85ecba16b28ccd1b280bd481970989f008
Brandon Levy <brandon.levy@mail.mcgill.ca>
2018-02-10 15:51:38 -0500

    added react routes and removed post stuff from index page

--- src/index.js
+++ src/index.js
@@ -1,23 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Post from './App';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-var post = {
-  title:"Dinosaurs are Awesome",
-  author:"Stealth Stegasaurus",
-  body:"Check out this property",
-  comments:["First!", "Great post", "Hire this author now!"],
-  authors:["Au1", "Au2", "Au3"]
-}
 
 
-ReactDOM.render(<Post
-  title={post.title}
-  author={post.author}
-  allAuthors={post.authors}
-  body={post.body}
-  comments={post.comments}
-  />, document.getElementById('root'));
+
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
