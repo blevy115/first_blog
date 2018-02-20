dd241572996003117e85678911d1ecd62dad2ba7
Brandon Levy <brandon.levy@mail.mcgill.ca>
2018-02-19 22:47:01 -0500

    added api fetches for poems and weather

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react';
 
 class Home extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      shakesPeare:""
+    }
+  }
+
+  componentDidMount() {
+    var base = this;
+    let poemApi = 'http://shakeitspeare.com/api/poem'
+    fetch(poemApi)
+      .then((response) => {
+        return response.json()
+      })
+      .then((json) => {
+        base.setState ({ shakesPeare: json.poem})
+      })
+      .catch((ex) => {
+        console.log('There was an error', ex);
+      })
+    }
+
   render() {
+    let poetry = this.state.shakesPeare;
+    if (this.state.shakesPeare){
+      return (
+        <div>
+        <h1>My favorite Shakespeare poem:</h1>
+        <h2>{poetry}</h2>
+        </div>
+      )
+    }
     return (
-      <div id='Home'>
-      <h1>Home Page</h1>
-        <p>Here is the Home Page </p>
+      <div>
+        <h1>My favorite Shakespeare poem:</h1>
+        <h2>Loading ...</h2>
       </div>
     )
   }
